0c0de21986fc9aac2d17546e7d29c9ff737d75d4
Flo <flo@flal.net>
2019-08-23 08:22:32 +0200

    awesome rc.lua

--- link.sh
+++ link.sh
@@ -3,3 +3,9 @@
 BASEDIR=$(dirname $(readlink -f "$0"))
 
 ln -s -i  "$BASEDIR/.Xresources" "$HOME/.Xresources"
+if [[ ! -e "$HOME/.emacs.d" ]]; then
+    mkdir "$HOME/.emacs.d"
+fi
+
+ln -s -i  "$BASEDIR/emacs/init.el" "$HOME/.emacs.d/init.el"
+ln -s -i  "$BASEDIR/awesome" "$HOME/.config/awesome"
