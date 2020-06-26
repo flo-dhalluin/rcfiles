a3eca55b413d0253d5a0377ff3e232da69a871de
Florent D'halluin <f.dhalluin@netheos.net>
2020-06-26 16:56:07 +0200

    i3 config

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDIR=$(dirname $(readlink -f "$0"))
 
@@ -9,3 +9,6 @@ fi
 
 ln -s -i  "$BASEDIR/emacs/init.el" "$HOME/.emacs.d/init.el"
 ln -s -i  "$BASEDIR/awesome" "$HOME/.config/awesome"
+
+ln -s -i "$BASEDIR/i3/i3" "$HOME/.config/i3"
+ln -s -i "$BASEDIR/i3/i3status" "$HOME/.config/i3status"
