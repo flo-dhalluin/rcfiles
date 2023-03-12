c0dc6054bcb47e6f8be615525a5e183b320373b8
Flo <flo@flal.net>
2023-03-12 15:01:28 +0100

    migrate emacs to use-package

--- emacs/init.el
+++ emacs/init.el
@@ -2,6 +2,24 @@
 ;; no startup / splash message
 (setq inhibit-startup-screen t)
 
+
+;; packages
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+
 ;; disabling menu and toolbar
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -9,67 +27,55 @@
 (show-paren-mode t)
 (ido-mode)
 
-;; MELPA
-(require 'package)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-(package-initialize)
-
-;; auto-install !
-(setq package-selected-packages
-      '(jedi
-	clojure-mode
-	paredit-mode
-	markdown-mode
-	rust-mode
-	cargo-mode
-	lsp-mode
-	lsp-ui
-	direnv
-	typescript-mode
-	company))
-
-(package-install-selected-packages)	
-
-(require 'lsp-mode)
-
-(add-hook 'prog-mode-hook 'direnv-mode)
-
-(add-hook 'rust-mode-hook 'lsp)
-(add-hook 'typescript-mode-hook 'lsp)
-;; -- python -- 
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
-
-;; -- clojure --
-;; enable paredit 
-(add-hook 'clojure-mode-hook 'paredit-mode)
-(add-hook 'clojurescript-mode-hook 'paredit-mode)
-;; company mode completion for cider
-(add-hook 'cider-repl-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'company-mode)
-
-
 ;; theming : 
 ;; inconsolata
 (set-frame-font "Inconsolata-12")
+;; tango-dark theme
 
-;; default dark-tango theme
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark))
- '(org-agenda-files '("~/Nextcloud/Documents/org/main.org"))
- '(package-selected-packages
-   '(direnv typescript-mode json-mode company nix-mode elixir-mode flymake-rust rust-mode lua-mode org-present presentation yaml-mode paredit clojure-mode cider jedi markdown-mode))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package which-key
+  :init (which-key-mode)
+  :diminish (which-key-mode)
+  :config (setq which-key-idle-delay 1))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :config (lsp-enable-which-key-integration t)
+  :init (setq lsp-keymap-prefix "C-c l"))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode))
+
+
+(use-package direnv
+  :config (direnv-mode))
+
+(use-package rust-mode
+  :hook (rust-mode . lsp-deferred))
+
+(use-package typescript-mode
+  :mode "\\.ts\\'"
+  :hook (typescript-mode .lsp-deferred))
+
+(use-package pyvenv
+  :config (pyvenv-mode 1))
+
+(use-package python-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred))
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+	      ("<tab" . company-complete-selection))
+        (:map lsp-mode-map
+	      ("<tab>" . company-indent-or-complete-common))
+  :custom
+   (company-minimum-prefix-length 1)
+   (company-idle-delay 0.0))
+
+
+;; do not pollute this one with custom stuff.. 
+(setq custom-file "~/.emacs-custom")
+(if (file-exists-p custom-file)
+    (load custom-file))
