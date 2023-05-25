e3a1af81c643c253b4b9c27a487b82f4fd45c3fa
Flo <f.dhalluin@netheos.net>
2023-05-25 10:57:30 +0200

    sway: initial config

--- link.sh
+++ link.sh
@@ -12,3 +12,7 @@ ln -s -i  "$BASEDIR/awesome" "$HOME/.config/awesome"
 
 ln -s -i "$BASEDIR/i3/i3" "$HOME/.config/i3"
 ln -s -i "$BASEDIR/i3/i3status" "$HOME/.config/i3status"
+
+ln -s -i "$BASEDIR/sway/sway" "$HOME/.config/sway"
+ln -s -i "$BASEDIR/sway/waybar" "$HOME/.config/waybar"
+ln -s -i "$BASEDIR/sway/kanshi" "$HOME/.config/kanshi"
